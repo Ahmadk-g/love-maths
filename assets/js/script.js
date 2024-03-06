b817a64e6f0e5936e02d9d829aab49fbe2c8d211
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-03-06 12:58:20 +0100

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,6 +90,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert (`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.aborting!`;
@@ -132,4 +136,10 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 * operand2; //to make sure we the answer is a whole number (integer)
+    document.getElementById('operand2').textContent = Math.random() > 0.5 ? operand1 : operand2; //to pick either operand1 or operand 2 randomly
+    document.getElementById('operator').textContent = "/";
 }
